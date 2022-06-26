c03c991e5cbc754185f515739d971da74f9cff5e
jocnn <ijann_@outlook.com>
2022-06-25 21:32:46 -0300

    se verifico el uso de plantillas y el contenido

--- pages/tests.py
+++ pages/tests.py
@@ -11,6 +11,13 @@ class HomepageTests(SimpleTestCase):
     response = self.client.get(reverse("home"))
     self.assertEqual(response.status_code, 200)
     
+  def test_template_name_correct(self):
+    response = self.client.get(reverse("home"))
+    self.assertTemplateUsed(response, "home.html")
+  
+  def test_template_content(self):
+    response = self.client.get(reverse("home"))
+    self.assertContains(response, "<h1>Home page</h1>")
 
 class AboutpageTests(SimpleTestCase):
   def test_url_exists_at_correct_location(self):
@@ -21,3 +28,10 @@ class AboutpageTests(SimpleTestCase):
     response = self.client.get(reverse("about"))
     self.assertEqual(response.status_code, 200)
   
+  def test_template_name_correct(self):
+    response = self.client.get(reverse("about"))
+    self.assertTemplateUsed(response, "about.html")
+    
+  def test_template_content(self):
+    response = self.client.get(reverse("about"))
+    self.assertContains(response, "<h1>About page</h1>")
